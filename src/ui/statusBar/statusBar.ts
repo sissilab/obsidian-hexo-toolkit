4571eef738cd3702e9074252965f2def9ffb9944
Xiangru <xiangru.au@gmail.com>
2024-10-30 13:42:18 +1100

    format

--- src/ui/statusBar/statusBar.ts
+++ src/ui/statusBar/statusBar.ts
@@ -2,21 +2,21 @@ import { PluginStatus } from "src/conversion/conversionState";
 
 export class StatusBar {
 
-    private baseMessage = 'Hexo: ';
+	private baseMessage = 'Hexo: ';
 
-    constructor(private readonly statusBarEl: HTMLElement) {
-    }
+	constructor(private readonly statusBarEl: HTMLElement) {
+	}
 
-    public addModClickable() {
-        this.statusBarEl.addClass('mod-clickable');
-    }
+	public addModClickable() {
+		this.statusBarEl.addClass('mod-clickable');
+	}
 
-    public removeModClickable() {
-        this.statusBarEl.removeClass('mod-clickable');
-    }
+	public removeModClickable() {
+		this.statusBarEl.removeClass('mod-clickable');
+	}
 
-    public display(status: PluginStatus) {
-        this.statusBarEl.setText(this.baseMessage + status);
-    }
+	public display(status: PluginStatus) {
+		this.statusBarEl.setText(this.baseMessage + status);
+	}
 
 }
